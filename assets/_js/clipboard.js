7756a20730a3469a3afac1d063ce2f3f9a3142a7
Anne Lee Steele <aleesteele@gmail.com>
2018-11-02 13:07:13 -0400

    switching branches

--- assets/_js/clipboard.js
+++ assets/_js/clipboard.js
@@ -1,28 +1,17 @@
-import ClipboardJS from 'clipboard'
+import * from 'clipboard'
 
 const Clipboard = () => {
-  console.log('looogggeeeddd')
   let linkIcon = document.querySelector('.icon-link')
   let pageURL = document.location.href
 
   if (!linkIcon) return
 
-  clipboardInit()
-
-  linkIcon.addEventListener('click', () =>
+  var clipboard = new Clipboard('.icon-link', {
     text: function() {
-      return 'clipboard.js is awesome!';
+        return 'clipboard.js is awesome!'
     }
-  )
-}
-
-const clipboardInit = () => {
-  let clipboard = new ClipboardJS('.icon-link')
-
-  clipboard.on('success', () => {
-    // let d = document.querySelector('.cite-page .btn')
-    // d.classList.add('copied')
   })
+
 }
 
 export default Clipboard
